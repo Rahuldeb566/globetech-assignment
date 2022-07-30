baa3c1437991c0fa0de53b7875d5a7023d64bbf1
rahuldeb566 <rahul566.rd@gmail.com>
2022-07-30 22:35:40 +0600

    Last update

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -25,7 +25,11 @@ const Home = () => {
                         <div className="sqr2">
                             <div className="sqr3">
                                 <div className="sqr4">
-                                
+                                    <div className="circle1"></div>
+                                    <div className="circle2"></div>
+                                    <div className="circle3"></div>
+                                    <div className="circle4"></div>
+                                    <div className="circle5"></div>
                                 </div>
                             </div>
                         </div>
